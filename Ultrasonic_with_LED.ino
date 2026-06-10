0b369d966b9f816dc8963f01c9af5c3c44e4bfb9
Mahbubul Islam <168621210+mahbubul03@users.noreply.github.com>
2026-06-10 16:40:19 +0600

    Add files via upload

---
+++ Ultrasonic_with_LED.ino
@@ -0,0 +1,47 @@
+const int trigPin = D5;
+const int echoPin = D6;
+
+const int ledr = D2;
+const int ledb = D0;
+
+
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+long duration;
+float distanceCm;
+float distanceInch;
+void setup() {
+  Serial.begin(115200);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  pinMode(ledr,OUTPUT);
+  pinMode(ledb,OUTPUT);
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_VELOCITY/2;  
+  distanceInch = distanceCm * CM_TO_INCH;
+
+if (distanceCm <= 5) {
+  digitalWrite(ledr, HIGH);  
+  digitalWrite(ledb, LOW); 
+  }
+else {
+  digitalWrite(ledr, LOW);  
+  digitalWrite(ledb, HIGH); 
+  }
+
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);  
+
+
+
+  delay(1000);
+}
